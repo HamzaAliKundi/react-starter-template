5a04002aef726db8b4b8bb06b6bf21f592291e4f
HamzaAliKundi <hamza.ale83@gmail.com>
2025-05-21 23:55:59 +0500

    redirect fixes

--- src/components/auth/sign-up.tsx
+++ src/components/auth/sign-up.tsx
@@ -145,7 +145,7 @@ const Signup = () => {
           <div className="text-center">
             <p className="text-gray-600 text-sm">
               Already have an account?{" "}
-              <Link to="/login" className="text-blue-600 hover:text-blue-800">
+              <Link to="/" className="text-blue-600 hover:text-blue-800">
                 Login
               </Link>
             </p>
